c3c894df071f21c129a28aba8ffe4f82a938eb00
Rajeshwaran S P <tuxrajesh@gmail.com>
2012-09-19 00:26:16 -0400

    Changed Button Images for Guess and Retry

--- gen/raj/game/cowsandbulls/R.java
+++ gen/raj/game/cowsandbulls/R.java
@@ -11,13 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int bg_button=0x7f020000;
-        public static final int bull=0x7f020001;
-        public static final int cow=0x7f020002;
-        public static final int giveup=0x7f020003;
-        public static final int guess=0x7f020004;
-        public static final int ic_action_search=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
+        public static final int bull=0x7f020000;
+        public static final int cow=0x7f020001;
+        public static final int guess=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int retry=0x7f020004;
     }
     public static final class id {
         public static final int bullCountText=0x7f070007;
