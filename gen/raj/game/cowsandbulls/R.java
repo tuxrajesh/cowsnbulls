9a3f8f2b648a641cf21ef380be3a96766d4ca386
Rajeshwaran S P <tuxrajesh@gmail.com>
2012-09-18 23:21:37 -0400

    Made changes to avoid unnecessary classes
    Removed the Welcome Screen to adhere to minimalism of design

--- gen/raj/game/cowsandbulls/R.java
+++ gen/raj/game/cowsandbulls/R.java
@@ -14,29 +14,26 @@ public final class R {
         public static final int bg_button=0x7f020000;
         public static final int bull=0x7f020001;
         public static final int cow=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int giveup=0x7f020003;
+        public static final int guess=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int bannerImage=0x7f070009;
         public static final int bullCountText=0x7f070007;
         public static final int bullImage=0x7f070006;
         public static final int cowCountText=0x7f070005;
         public static final int cowImage=0x7f070004;
-        public static final int futureButton=0x7f07000c;
-        public static final int giveUpButton=0x7f070002;
-        public static final int guessButton=0x7f070001;
-        public static final int guessListView=0x7f070003;
+        public static final int guessButton=0x7f070002;
+        public static final int guessListView=0x7f070000;
         public static final int guessText=0x7f070008;
-        public static final int instructionButton=0x7f07000b;
-        public static final int menu_settings=0x7f07000d;
-        public static final int newGameButton=0x7f07000a;
-        public static final int userGuessEditText=0x7f070000;
+        public static final int menu_settings=0x7f070009;
+        public static final int retryButton=0x7f070003;
+        public static final int userGuessEditText=0x7f070001;
     }
     public static final class layout {
-        public static final int gameplay=0x7f030000;
+        public static final int game=0x7f030000;
         public static final int guessitem=0x7f030001;
-        public static final int welcome=0x7f030002;
     }
     public static final class menu {
         public static final int game_play=0x7f060000;
@@ -52,8 +49,8 @@ public final class R {
         public static final int image_bull_descripton=0x7f04000b;
         public static final int image_cow_description=0x7f04000a;
         public static final int menu_settings=0x7f040007;
-        public static final int title_activity_gameplay=0x7f040009;
-        public static final int title_activity_welcome=0x7f040008;
+        public static final int title_activity_cowsnbulls=0x7f040008;
+        public static final int title_activity_game=0x7f040009;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
